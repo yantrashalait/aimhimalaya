fd780a02b61ebd649d64aaacd4079861aeb66385
sanip <saneprijal@gmail.com>
2019-12-05 15:49:52 +0545

    popular scroll

--- trek/templatetags/filters.py
+++ trek/templatetags/filters.py
@@ -29,3 +29,29 @@ def get_review_count(package):
     return Review.objects.filter(package_id=package, accepted=True).count()
 
 
+@register.filter
+def separate_four(popular):
+    total = len(popular)
+    if total < 4:
+        return range(1)
+    else:
+        if(total / 4) == 0:
+            return range((total / 4))
+        else:
+            return range((total // 4) + 1)
+
+
+@register.filter
+def block_popular(popular, index=None):
+    if index == 1:
+        return Package.objects.filter(speciality='Popular')[:4]
+
+    if index > 1:
+        start = (index - 1) * 4
+        end = start * 2
+        print(start, end)
+        print(Package.objects.filter(speciality='Popular')[start:end])
+        return Package.objects.filter(speciality='Popular')[start:end]
+
+
+
